e1f1de6bafe37831e7b28496a8c38b63af8f487f
Victor Elias <victorgelias@gmail.com>
2016-11-15 18:23:24 -0200

    Remove /stadium/collect API

--- webapp/routes/stadium.js
+++ webapp/routes/stadium.js
@@ -18,23 +18,4 @@ router.get('/:autoStadiumId/pokemons', function(req, res, next) {
       }, next)
 })
 
-router.post('/:autoStadiumId/collect', function(req, res, next) {
-  var updateDoc = {}
-  let items = req.stadium.items;
-  ['pokeball', 'greatball', 'revive', 'lure']
-      .filter(k => k in items && items[k] > 0)
-      .forEach(k => updateDoc["bag." + k] = items[k])
-
-  updateDoc = {$inc: updateDoc}
-  req.db.get('user').findOneAndUpdate({_id: req.body.userId}, updateDoc, 'bag',
-      function(err, user) {
-        if (!user) {
-          res.status(404)
-          return next(new Error("User not found"))
-        }
-        res.data = {bag: user.bag}
-        next(err)
-      })
-})
-
 module.exports = router
